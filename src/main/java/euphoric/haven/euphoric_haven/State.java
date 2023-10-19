92bd39156ae2307e2cd64ed79e4fd7877146318a
Deborah Alvina <deborahxp975@gmail.com>
2023-10-19 20:48:58 +0530

    Fixed bugs

--- src/main/java/euphoric/haven/euphoric_haven/State.java
+++ src/main/java/euphoric/haven/euphoric_haven/State.java
@@ -1,1 +1,26 @@
+package euphoric.haven.euphoric_haven;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.util.List;
+
+public class State {
+    private final StringProperty name;
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public State() {
+        name = new SimpleStringProperty(null);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+}
